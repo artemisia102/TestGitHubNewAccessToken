235d6d964268e1f1f47c357381ea271a6221d605
Mira <walkingslow328@gmail.com>
2025-02-24 11:05:43 +1100

    Initial Commit

---
+++ TestGitHubNewAccessToken/TestGitHubNewAccessTokenApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitHubNewAccessTokenApp.swift
+//  TestGitHubNewAccessToken
+//
+//  Created by Mira on 24/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestGitHubNewAccessTokenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
